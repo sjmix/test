09a57e5700a6ea74f203a8809a18a2bf935e6461
S.jmix <S.jmix>
2017-03-30 20:34:18 +0300

    changes

--- atest/src/app/app.component.ts
+++ atest/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works! eeeee!!!!';
+  title = 'app works! auto-build test';
 }
